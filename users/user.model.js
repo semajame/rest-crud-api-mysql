0522eb867cd26cc49a489696a51383b0ac39995e
semajame <jamesanquillano@gmail.com>
2024-02-23 10:06:19 +0800

    update

--- users/user.model.js
+++ users/user.model.js
@@ -6,10 +6,7 @@ function model(sequelize) {
   const attributes = {
     email: { type: DataTypes.STRING, allowNull: false },
     passwordHash: { type: DataTypes.STRING, allowNull: false },
-    title: { type: DataTypes.STRING, allowNull: false },
-    firstName: { type: DataTypes.STRING, allowNull: false },
-    lastName: { type: DataTypes.STRING, allowNull: false },
-    role: { type: DataTypes.STRING, allowNull: false },
+    username: { type: DataTypes.STRING, allowNull: false },
   };
 
   const options = {
